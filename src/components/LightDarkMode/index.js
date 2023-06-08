6cf4aac68fdec390a99b7ff808f8433cf6a5d0ee
ppunem <prathyusha2094@gmail.com>
2023-06-08 16:47:48 +0000

    semi

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -2,31 +2,40 @@
 import {Component} from 'react'
 import './index.css'
 
-class LightDarkMode extends Component{
-    state={btnText:Light Mode,mode:dark-mode,btnMode:dark-button}
+class LightDarkMode extends Component {
+  state = {
+    cardBg: 'dark-bg',
+    head: 'white-head',
+    btnBg: 'white-button',
+    btnText: 'Light Mode',
+  }
 
-    changeMode=()=>{
-        this.state(prevState=>{
-            return ({btnText==="Light Mode"?{btnText:Dark Mode}:{btnText:Light Mode}})
-            return ({mode==="dark-mode"?{mode:light-mode}:{mode:dark-mode}})
-            return ({btnMode===dark-button?{btnMode:light-mode}:{btnMode:dark-mode}})
-        })
-    }
+  changeMode = () => {
+      this.setState(if (cardBg==="dark-bg"){
+          return ({cardBg:"white-bg",head:"dark-head",btnBg:"dark-button",btnText:"Dark Mode"})
+      }
+      else{return ({cardBg: "dark-bg",
+    head: "white-head",
+    btnBg: "white-button",
+    btnText: "Light Mode"})})
+  }
 
-    render(){
-        const {btnText,mode,btnMode}=this.state
+  render() {
+    const {cardBg, head, btnBg, btnText} = this.state
 
-        return (
-            <div className="bg">
-                <div className={mode}>
-                    <h1>Click To Change Mode</h1>
-                    <button type="button" className={btnMode} onClick={this.changeMode}>{btnText}</button>
-                </div>
-            </div>
-            
-        )
-    }
-
+    return (
+      <div className="bg">
+        <div className={cardBg}>
+          <h1 className={head}>Click To Change Mode</h1>
+          <div className="btn">
+            <button type="button" className={btnBg} onClick={this.changeMode}>
+              {btnText}
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default LightDarkMode
